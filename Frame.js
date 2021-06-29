87245ad0bc1ef5abd8c3c4f4d6621b86f69e4b95
himangshu06 <76275772+himangshu06@users.noreply.github.com>
2021-06-29 20:11:57 +0530

    Add files via upload

---
+++ Frame.js
@@ -0,0 +1,43 @@
+noseX = 0;
+noseY = 0;
+function preload(){
+ clownnose = loadImage('https://i.postimg.cc/PxFvYgkv/l1.png');
+}
+
+function setup(){
+canvas = createCanvas(300,300);
+canvas.center();
+video = createCapture(VIDEO);
+video.size(300,300);
+video.hide();
+poseNET = ml5.poseNet(video,modelLoaded);
+poseNET.on('pose',gotPoses);
+}
+
+function modelLoaded(){
+    console.log("poseNet is intilized");
+}
+
+function draw(){
+image(video,0,0,300,300);
+image(clownnose,noseX,noseY,75,75);
+}
+
+function gotPoses(results){
+     if(results.length > 0){
+          console.log(results);
+          noseX = results[0].pose.nose.x-30;
+          noseY = results[0].pose.nose.y+10;
+          console.log("noseX = "+noseX);
+            console.log("noseY = "+noseY);
+        }
+         }
+
+function takesnapshot(){
+    save("Filter.png");
+}
+
+
+
+    
+
